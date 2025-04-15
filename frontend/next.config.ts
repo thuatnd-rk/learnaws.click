b9c36c25a75f9f34efbfa3ec13a4af03742e4490
Thuat. Nguyen Danh - CTS TE.CTX.SD HN <ndthuat@cmc.com.vn>
2025-04-15 11:48:13 +0700

    fix: fix error when build docker img

--- frontend/next.config.ts
+++ frontend/next.config.ts
@@ -2,6 +2,10 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   output: 'standalone',
+  eslint: {
+    // Don't run ESLint during production builds
+    ignoreDuringBuilds: true,
+  },
 };
 
 export default nextConfig;
